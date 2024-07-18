db405a745d5207ce03aa533f751a8daa8d0ce233
AnthonyL7 <anthonylanda022@gmail.com>
2024-07-18 11:15:58 -0700

    Make grids

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,11 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
 
-  let grid = document.getElementById('grid');
+  let container = document.getElementById('container');
+  let gridSize = 16;
 
-  for (let i = 0; i < 16 * 16; i++) {
+  for (let i = 0; i < gridSize * gridSize; i++) {
     let cell = document.createElement('div');
-    cell.className = 'grid-item';
-    grid.appendChild(cell);
-  }
+    cell.classList.add('cell');
+    container.appendChild(cell);
 
+    cell.addEventListener('mouseenter', () => {
+      cell.classList.add('hovered');
+    });
+
+    cell.addEventListener('mouseleave', () => {
+      cell.classList.remove('hovered');
+    });
+  }
 });
