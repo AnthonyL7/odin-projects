ed0cfd726623873ec7eead00d3380009cb035a3f
AnthonyL7 <anthonylanda022@gmail.com>
2024-12-31 08:09:49 -0800

    Add option for user input and adjust grid size

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,17 +1,42 @@
 const container = document.querySelector('#container');
+const gridSizeInput = document.querySelector('#gridSize')
+const createButton = document.querySelector('#createButton')
 
 
-for (let i = 1; i <= 256; i++) {
-  const square = document.createElement('div');
-  square.style.width = '20px'
-  square.style.height = '20px'
-  square.style.backgroundColor = 'white'
-  square.style.border = '2px solid black'
-  square.style.boxSizing = 'border-box'
+function generateGrid(size) {
+  const containerSize = 500;
+  const borderSize = 1;
+  const squareSize = (containerSize / size) - 2 * borderSize;
 
-  square.addEventListener('mouseover', () => {
+  container.innerHTML = ''
+
+  container.style.width = `${containerSize}px`;
+  container.style.height = `${containerSize}px`;
+
+  for (let i = 1; i < size * size; i++) {
+    const square = document.createElement('div');
+    square.style.width = `${squareSize}px`
+    square.style.height = `${squareSize}px`
+    square.style.backgroundColor = 'white'
+    square.style.border = `${borderSize}px solid black`
+    square.style.boxSizing = 'border-box'
+
+    square.addEventListener('mouseover', () => {
     square.style.backgroundColor = 'blue';
-  })
-  container.appendChild(square)
+    })
+    container.appendChild(square)
+  }
+
 }
+
+createButton.addEventListener('click', () =>{
+  const gridSize = parseInt(gridSizeInput.value);
+  if (gridSize >= 1 && gridSize <= 100) {
+    generateGrid(gridSize);
+  } else {
+    alert('Enter a number between 1 and 100')
+  }
+});
+
+generateGrid(16)
 
