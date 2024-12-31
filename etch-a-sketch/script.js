ed0ac1c222d82a5360f7cb51227817d0c9fd78dd
AnthonyL7 <anthonylanda022@gmail.com>
2024-12-31 12:59:20 -0800

    Add scalability potential

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,30 +1,27 @@
 const container = document.querySelector('#container');
-const gridSizeInput = document.querySelector('#gridSize')
-const createButton = document.querySelector('#createButton')
+const gridSizeInput = document.querySelector('#gridSize');
+const createButton = document.querySelector('#createButton');
 
 
 function generateGrid(size) {
-  const containerSize = 500;
-  const borderSize = 1;
-  const squareSize = (containerSize / size) - 2 * borderSize;
 
   container.innerHTML = ''
 
-  container.style.width = `${containerSize}px`;
-  container.style.height = `${containerSize}px`;
+  container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 
-  for (let i = 1; i < size * size; i++) {
+  for (let i = 0; i < size * size; i++) {
+
+
     const square = document.createElement('div');
-    square.style.width = `${squareSize}px`
-    square.style.height = `${squareSize}px`
-    square.style.backgroundColor = 'white'
-    square.style.border = `${borderSize}px solid black`
-    square.style.boxSizing = 'border-box'
+    square.style.backgroundColor = 'white';
+    square.style.border = '1px solid black';
+    square.style.boxSizing = 'border-box';
 
     square.addEventListener('mouseover', () => {
     square.style.backgroundColor = 'blue';
     })
-    container.appendChild(square)
+    container.appendChild(square);
   }
 
 }
@@ -34,9 +31,9 @@ createButton.addEventListener('click', () =>{
   if (gridSize >= 1 && gridSize <= 100) {
     generateGrid(gridSize);
   } else {
-    alert('Enter a number between 1 and 100')
+    alert('Enter a number between 1 and 100');
   }
 });
 
-generateGrid(16)
+generateGrid(16);
 
