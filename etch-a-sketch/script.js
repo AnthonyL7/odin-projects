1aa8c0ecb96bb6677106adff630d78cff8171478
AnthonyL7 <anthonylanda022@gmail.com>
2024-12-31 07:13:45 -0800

    Add css styles and center grid

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,19 +1,17 @@
 const container = document.querySelector('#container');
-container.style.display = 'flex'
-container.style.flexWrap = 'wrap'
-container.style.justifyContent = 'center'
-container.style.alignContent = 'center';
-container.style.width = '320px';
-container.style.height = '320px';
+
 
 for (let i = 1; i <= 256; i++) {
   const square = document.createElement('div');
-  square.textContent = 'div';
-  square.style.backgroundColor = 'red'
   square.style.width = '20px'
-  square.style.length = '20px'
+  square.style.height = '20px'
+  square.style.backgroundColor = 'white'
+  square.style.border = '2px solid black'
+  square.style.boxSizing = 'border-box'
 
-
+  square.addEventListener('mouseover', () => {
+    square.style.backgroundColor = 'blue';
+  })
   container.appendChild(square)
 }
 
