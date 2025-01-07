1118e817d0b3f86c2f6eebd1a4870d75c986d6be
AnthonyL7 <anthonylanda022@gmail.com>
2025-01-07 14:25:01 -0800

    Add div containers and multiple function for each calculator function

---
+++ calculator/script.js
@@ -0,0 +1,29 @@
+// Sum Function
+function sum(a,b) {
+  return a + b
+}
+console.log(sum(2,2))
+
+// Subtract Function
+function subtract(a,b) {
+  return a - b
+}
+console.log(subtract(4,2))
+
+// Multiply Function
+function multiply(a,b) {
+  return a * b
+}
+console.log(multiply(2,2))
+
+// Divide Function
+function divide(a,b) {
+  return a / b
+}
+console.log(divide(10,5))
+
+
+//Function Operate
+function operate(operator, num1, num2) {
+  
+}
